c09ff7122150485f4abefb6672fa8da7eea7a776
Whutai <taiphantmt@gmail.com>
2024-10-22 14:09:57 +0700

    update

---
+++ data/data.js
@@ -0,0 +1,13 @@
+export const data = [
+  {
+    name: "1",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S7de20e067c9741bbb171e3729d1a46adk.jpg_350x350.jpg",
+    shortlink: "",
+    customlink:
+      "https://hutaw.com/products/creative-automatic-page-turning-clock"
+  }
+];
