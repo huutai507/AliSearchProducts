277d3fb38db21d3541ca8949761463bc42564427
Whutai <taiphantmt@gmail.com>
2024-10-22 15:28:45 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,13 +1,122 @@
 export const data = [
   {
+    name: "12",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Hac38d9d1c3d34329a2d90431f0f6b171i.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_DdxMmQZ"
+  },
+  {
+    name: "11",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S56839b0b186e43f3969865228ccf4199T.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_DBxaraN"
+  },
+  {
+    name: "10",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Sdb66c45e4a98482581b2dad39adb44d1e.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_DlwTGjT"
+  },
+  {
+    name: "9",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S3838da4194c54f8ebea60e60aff99631n.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_DDToOhn"
+  },
+  {
+    name: "8",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Sdd71837c61474983b0dc8f21a502e833D.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_DF5yzBR"
+  },
+  {
+    name: "7",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Sa7b53f04da72486e8ebdf6c54f101577w.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_DcQnnS5"
+  },
+  {
+    name: "6",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/HTB1oAonajDuK1Rjy1zjq6zraFXaX.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_DF6J0Cz"
+  },
+  {
+    name: "5",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S7c8a5d6d6c0440ecba5a66ea516cbf3fd.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_Dm4BVPJ"
+  },
+  {
+    name: "4",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Sc7415006bcf046e5b0b402b2dc19885aI.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_De19ndx"
+  },
+  {
+    name: "3",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S5c40eeda8c534514b216ae78e2b8ffc1Y.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_De19ndx"
+  },
+  {
+    name: "2",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Sb759428f2376441985d2a5ef26e454570.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_Dnq6OOZ"
+  },
+  {
     name: "1",
     podlink: "",
     usalink: "",
     aliexpress: "",
     imglink:
-      "//ae01.alicdn.com/kf/S7de20e067c9741bbb171e3729d1a46adk.jpg_350x350.jpg",
+      "//ae01.alicdn.com/kf/S644673d3a03d4d789297711a29148c700.jpg_350x350.jpg",
     shortlink: "",
-    customlink:
-      "https://hutaw.com/products/creative-automatic-page-turning-clock"
+    customlink: "https://s.click.aliexpress.com/e/_DDrciS1"
   }
 ];
