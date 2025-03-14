b3ad2d67b8f1f7e563006575fce02ff7b01b0385
Whutai <taiphantmt@gmail.com>
2025-03-14 11:11:51 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,12 +1,162 @@
 export const data = [
   {
+    name: "16",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S1dc644044e3d4cf3a2764a829ee8be1dF.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_oFiwkE1"
+  },
+  {
+    name: "15",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S5567694ca8aa4106b7381b740f2752c1j.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_oknUcvn"
+  },
+  {
+    name: "14",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S794fd002d64c4d62896edd4aa0a892ddj.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_oBvDJeV"
+  },
+  {
+    name: "13",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Se59fcf430fff4fa7b6391a55c8fbd40fB.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_okidDFJ"
+  },
+  {
+    name: "12",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Sd8a89f64a8d24e1b9c8e4242af65c942r.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_on5HvLJ"
+  },
+  {
+    name: "11",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S1cf7801aaeae47adb501aaad98008634n.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_oDZVHEd"
+  },
+  {
+    name: "10",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Sb0cdcf3ecb734c949f0223d0c07372e01.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_opNblm9"
+  },
+  {
+    name: "9",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S48aefd37662d47c49d9a7565cb8f6071R.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_onGUizn"
+  },
+  {
+    name: "8",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S564f1dcfbf154f868f1040cf0f5dfdbeA.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_opf36S1"
+  },
+  {
+    name: "7",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S29f623a10479420d9d2dc1fd1b5b60e6v.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_okZXMtr"
+  },
+  {
+    name: "6",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Sd481788f9ebf47b39b9a4a37dcdf59adF.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_omehyqp"
+  },
+  {
+    name: "5",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S96037f9ae3af410aae4cc95a2b5411c7Q.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_olrOqkz"
+  },
+  {
+    name: "4",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S3ef8627f1f194976bef1dab627f8a1a69.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_okSJ1Jf"
+  },
+  {
+    name: "3",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Sc1c990632572477ba71ccdd5a6f8f3bdi.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_oEQ0ULj"
+  },
+  {
+    name: "2",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Sd2b76f7390394fa39aa44fab042526c7o.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_on6JaJL"
+  },
+  {
     name: "1",
     podlink: "",
     usalink: "",
     aliexpress: "",
     imglink:
-      "//ae01.alicdn.com/kf/S644673d3a03d4d789297711a29148c700.jpg_350x350.jpg",
+      "//ae01.alicdn.com/kf/S87a088f175da4a5689b4d6170f18ed14U.jpg_350x350.jpg",
     shortlink: "",
-    customlink: "https://s.click.aliexpress.com/e/_DDrciS1"
+    customlink: "https://s.click.aliexpress.com/e/_omTsY7j"
   }
 ];
